f7ab613be9ae685a8655ccce87399de4a0e48a59
Carles Conesa Mañosa <a251158cc@correu.escoladeltreball.org>
2026-04-07 18:24:59 +0200

    Implement Refillable public interface

---
+++ src/main/java/com/vehicleInterface/Refillable.java
@@ -0,0 +1,11 @@
+package com.vehicleInterface;
+
+public interface Refillable {
+
+    public static final int CAR = 100;
+    public static final int MOTO = 50;
+    public static final int TRUCK = 200;
+    public static final int PRICE = 1;
+
+    public boolean refill();
+}
